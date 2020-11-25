6615df84524a750bf19b30c89e252dbe33c93de3
linyankun <491049029@qq.com>
2020-11-25 16:07:13 +0800

    Initial commit

---
+++ app/src/main/java/com/lyk/androidstudy/ui/fragment/FragmentStudyFragment.kt
@@ -0,0 +1,28 @@
+package com.lyk.androidstudy.ui.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.lyk.androidstudy.R
+
+class FragmentStudyFragment : Fragment() {
+
+    private val viewModel by viewModels<FragmentStudyViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_study_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/lyk/androidstudy/ui/fragment/FragmentStudyViewModel.kt
@@ -0,0 +1,7 @@
+package com.lyk.androidstudy.ui.fragment
+
+import androidx.lifecycle.ViewModel
+
+class FragmentStudyViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
